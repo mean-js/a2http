cf16e363f7b8d0959434a0fc0c208c9185b265e4
santosh <santosh.ece06@gmail.com>
2018-05-06 04:22:21 +0530

    post content.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title: string = 'DACBOOK';
+  public postLabel: string = 'Heyy, Watsapp...'; 
+  public post: string = '';
+
+  public postItemList: any[] = [];
+
+  postContent() {
+    const newPost = {'post' : this.post, 'type': 'text'};
+    this.postItemList.push(newPost);
+    
+    this.post = '';
+  }
 }
