d9421cd2393b6b34abe148bad4363835edf18104
santosh <santosh.ece06@gmail.com>
2018-05-06 04:52:55 +0530

    image and video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,40 @@ export class AppComponent {
 
   public postItemList: any[] = [];
 
-  postContent() {
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+  public postContent(): void {
     const newPost = {'post' : this.post, 'type': 'text'};
     this.postItemList.push(newPost);
-    
+
     this.post = '';
+  }
+
+
+  public postImage(fileRef): void {
+    try {
+      const currentFile = fileRef.files[0];
+      const srcUrl =  URL.createObjectURL(currentFile);
+      const src =  this.domSanitizer.bypassSecurityTrustResourceUrl(srcUrl);
+
+      const newPost = {'type': 'image', 'src': src};
+      this.postItemList.push(newPost);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  public postVideo(fileRef): void {
+    try {
+      const currentFile = fileRef.files[0];
+      const srcUrl =  URL.createObjectURL(currentFile);
+      const src =  this.domSanitizer.bypassSecurityTrustResourceUrl(srcUrl);
+
+      const newPost = {'type': 'video', 'src': src};
+      this.postItemList.push(newPost);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
